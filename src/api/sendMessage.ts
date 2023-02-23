e229d453b3a53bbb028755b20441529c45c02b89
SpiritKyiv <baolt.king@gmail.com>
2023-02-23 19:28:47 +0200

    add https agent

--- src/api/sendMessage.ts
+++ src/api/sendMessage.ts
@@ -1,4 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
+import https from 'https';
+
+const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
 interface UserData {
   name: string;
@@ -8,7 +11,7 @@ interface UserData {
 
 export const postDataMessage = async (data: UserData): Promise<AxiosResponse> => {
   try {
-    const response = await axios.post('http://54.175.253.210:8080/users', data);
+    const response = await axios.post('http://54.175.253.210:8080/users', data, { httpsAgent });
     return response;
   } catch (error) {
     console.error(error);
